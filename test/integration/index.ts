61ec86d3a08e710ce113e9d5e9befe8afe9d170a
Enrico <hey@enricosola.dev>
2024-03-17 02:58:57 +0100

    Added integration tests related to portfolio section.

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,6 +1,7 @@
 import Selenium from './support/Selenium';
 import './user_signup';
 import './user_login';
+import './portfolio_crud';
 
 process.on('uncaughtException', () => Selenium.disposeWebDriver().then(process.exit(0)));
 process.on('SIGINT', () => Selenium.disposeWebDriver().then(process.exit(0)));
